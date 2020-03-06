8a9f9ddebcbd50d2433f6356b00b7fd6dfbf7fb9
mhg99 <mikeguan1999@gmail.com>
2020-03-06 18:01:26 -0500

    Added thermistor code and arduino plot in python

---
+++ GUICode/arduino_plotter.py
@@ -0,0 +1,25 @@
+import serial
+import matplotlib.pyplot as plt
+import numpy as np
+plt.ion()
+fig = plt.figure()
+
+
+i = 0
+x = list()
+y = list()
+i = 0
+ser = serial.Serial('COM8', 9600)
+ser.close()
+ser.open()
+while True:
+
+    data = ser.readline()
+    print(data.decode())
+    x.append(i)
+    y.append(data.decode())
+
+    plt.scatter(i, data.decode())
+    i += 1
+    plt.show()
+    plt.pause(0.0001)  # Note this correction
