606fc11ee2471ac24e06c172db41d57cfa336ffa
Ronin Sharma <rrs234@cornell.edu>
2021-09-11 01:53:40 -0400

    updated website

--- website-gui/src/components/Navbar.jsx
+++ website-gui/src/components/Navbar.jsx
@@ -75,7 +75,6 @@ export default class Navbar extends Component {
 
                     <div class="md:hidden" id="mobile-menu">
                     <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-
                         <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
 
                         <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
