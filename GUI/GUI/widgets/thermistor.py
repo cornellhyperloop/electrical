7b9b0fb4bd1491e068a158cb4e22d4050cab8dd5
Kelvin <klw248@cornell.edu>
2021-11-19 18:13:43 -0500

    thermistor text color

--- GUI/GUI/widgets/thermistor.py
+++ GUI/GUI/widgets/thermistor.py
@@ -3,11 +3,12 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import serial
 
+
 class Thermistor(QWidget):
     def __init__(self, parent=None):
         super(Thermistor, self).__init__(parent)
         self.initUI()
-        self.arduino = serial.Serial("/dev/cu.usbmodem143301")
+        self.arduino = serial.Serial("/dev/cu.usbmodem143201")
         self.timer = QTimer(self, timeout=self.update)
         self.timer.start(1000)
 
@@ -16,6 +17,7 @@ class Thermistor(QWidget):
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.temp)
+        self.temp.setStyleSheet("background-color : rgb(143,255,91)")
         self.setLayout(vbox)
 
     def update(self):
