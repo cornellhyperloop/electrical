33ae5922309cddfbee42be0f9959b076c435b9dc
rwm275 <rwm275@cornell.edu>
2021-11-19 14:40:50 -0500

    thermistor live data

--- GUI/GUI/widgets/thermistor.py
+++ GUI/GUI/widgets/thermistor.py
@@ -1,25 +1,24 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-
-
+import serial
 
 class Thermistor(QWidget):
     def __init__(self, parent=None):
         super(Thermistor, self).__init__(parent)
         self.initUI()
+        self.arduino = serial.Serial("/dev/cu.usbmodem143301")
+        self.timer = QTimer(self, timeout=self.update)
+        self.timer.start(1000)
 
     def initUI(self):
-        avgtemp = QLabel(self)
-        with open('temp.txt') as f:
-            lines = f.readlines()
-
-        temp = []
-        for t in lines:
-            temp.append(float(t))
-        temp = round(sum(temp) / len(temp), 3)
-        avgtemp.setText("Avg temp: " + str(temp))
+        self.temp = QLabel(self)
 
         vbox = QVBoxLayout()
-        vbox.addWidget(avgtemp)
+        vbox.addWidget(self.temp)
         self.setLayout(vbox)
+
+    def update(self):
+        line = self.arduino.readline().decode()
+        if line:
+            self.temp.setText(line)
