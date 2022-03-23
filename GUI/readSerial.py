638f9ce60a598baf5d0a2e24f77f60d7079cfceb
Morgan Lo <morganlo@dhcp-vl2041-13718.eduroam.cornell.edu>
2022-03-22 20:27:49 -0400

    updated python and arduino script for JSON

--- GUI/readSerial.py
+++ GUI/readSerial.py
@@ -2,13 +2,13 @@ import serial
 import time
 
 # Update based on the value the Arduino IDE indicates
-serialConn = "/dev/cu.usbmodem143201"
+serialConn = "/dev/cu.usbmodem1401"
+ser= serial.Serial(serialConn)
 
 startTime = time.time()
 
-while time.time() - startTime < 5: # continue for 5 secconds
-    value = serialConn.readline.decode() # get the value written to the serial moniitor
+while time.time() - startTime < 1: # continue for 5 secconds
+    value = ser.readline().decode() # get the value written to the serial moniitor
     
     if value: # check if there's a new value
         print(value)
-
