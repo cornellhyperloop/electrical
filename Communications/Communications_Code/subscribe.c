847aef0b58b619dfb1de14c2b04023a32269029e
Michael Guan <mikeguan1999@gmail.com>
2022-03-28 16:57:36 -0400

    Reading to json file

--- Communications/Communications_Code/subscribe.c
+++ Communications/Communications_Code/subscribe.c
@@ -2,6 +2,8 @@
 #include <zcm/zcm.h>
 #include <sensor_info_t.h>
 
+void writeJson(char* fileName, const sensor_info_t *msg, size_t size);
+
 void callback_handler(const zcm_recv_buf_t *rbuf, const char *channel, const sensor_info_t *msg, void *usr)
 {
 	printf("Received a message on channel '%s'\n", channel);
@@ -9,6 +11,37 @@ void callback_handler(const zcm_recv_buf_t *rbuf, const char *channel, const sen
 	printf("msg->fiducial_detector_value = '%f'\n", msg->fiducial_detector_value);
 	printf("msg->thermistor_value = '%f'\n", msg->thermistor_value);
 	printf("\n");
+
+	writeJson("test.json", msg, 5);
+
+}
+
+// void writeJson(char* fileName, int* arr, size_t size) {
+void writeJson(char* fileName, const sensor_info_t *msg, size_t size) {
+	size_t i;
+    FILE *fp = fopen (fileName, "w+"); /* open file for writing */
+
+    if (!fp) {  /* validate file is open, or throw error */
+        fprintf (stderr, "writeJson() error: file open failed '%s'.\n", 
+                fileName);
+        return;
+    }
+	// TODO : Add other sensor fields later
+  // {
+	// 	"Temperature": {
+	// 		"temperature": 100
+	// 	}
+	// }
+
+		fprintf (fp, "{ \n\"Temperature\" : \n{");
+		fprintf (fp, "\"temperature\" : ");
+		//Print value
+		fprintf (fp, "%f", msg->thermistor_value);
+
+		fprintf (fp, "}\n");
+		fprintf(fp, "}\n");
+
+    fclose (fp);
 }
 
 int main(int argc, char *argv[])
