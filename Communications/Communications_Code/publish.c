df20c787b78e7faac4ff5a50c392e245fb5c58fd
Vanshaj24 <56195748+Vanshaj24@users.noreply.github.com>
2022-03-28 17:47:43 -0400

    Fix merge conflicts

--- Communications/Communications_Code/publish.c
+++ Communications/Communications_Code/publish.c
@@ -1,12 +1,17 @@
 #include <unistd.h>
 #include <zcm/zcm.h>
 #include <sensor_info_t.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
 	zcm_t *zcm = zcm_create("udpm://234.255.76.67:7667?ttl=1");
-
+	FILE *serialPort;
+	system( "MODE COM4: BAUD=9600 PARITY=n DATA=8 STOP=1" );
+  serialPort = fopen("COM4:", "r" );
 	sensor_info_t msg;
+<<<<<<< Updated upstream
 
 	// struct sensor_info_t
 	// {
@@ -30,6 +35,11 @@ int main(int argc, char *argv[])
 	msg.temperature = 7.0; //TODO: Get value from arduino
 	msg.proximity = 8.0;
 	msg.distance = 9.0;
+=======
+	msg.velocity = 10;
+	msg.fiducial_detector_value = 20;
+	msg.thermistor_value = serialPort;
+>>>>>>> Stashed changes
 
 	while (1) {
 		sensor_info_t_publish(zcm, "SENSOR_INFO", &msg);
