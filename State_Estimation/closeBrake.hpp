32d87ba7acd30b7cfa2f22efa782410e56fab531
ridhitbhura <ridhitbhura1234@gmail.com>
2022-10-17 18:11:58 -0400

    Header files reformatted

---
+++ State_Estimation/closeBrake.hpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Implement closeBrakes functionality by creating a new .cpp file and enable
+the closeBrakes sensor implementation /circuit
+**/
+bool closeBrakeMain();
+
+// Implement killing power to all components of the pod
+void killPower();
