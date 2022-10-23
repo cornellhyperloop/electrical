0a03d72e61aeadba71104fc34d5ce73dbb8f9274
Ronin Sharma <rrs234@cornell.edu>
2022-10-22 23:48:36 -0400

    added functionality for two live data plots

--- GUI/utils/body.py
+++ GUI/utils/body.py
@@ -2,8 +2,8 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from widgets import *
+from constants import *
 import pyqtgraph as pg
-
 
 class Body(QWidget):
     def __init__(self, *args, **kwargs):
@@ -33,13 +33,19 @@ class Body(QWidget):
         bottom_left.addWidget(speed)
 
         # temporary graph
-        self.temporary = pg.PlotWidget()
+        self.temporary_graph = pg.PlotWidget()
         hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
-        self.temporary.resize(self.width, self.height / 4)
+        self.temporary_graph.resize(self.width, self.height / 4)
 
-        self.plot_buttons = PlotButtons(self.temporary)
+        self.plot_buttons = PlotButtons(self.temporary_graph)
         plot_button_splitter.addWidget(self.plot_buttons)
+
+        # Data for plots
+        self.x_data = [[], []]
+        self.y_data = [[], []]
+        self.current_plot_indices = [0,0]
+        self.current_plot_values = [[0,0], [0,0]] # x,y for each plot
 
         prox_sensors = ProximitySensor()
         # battery = Battery()
@@ -61,7 +67,7 @@ class Body(QWidget):
 
         #temporary.plot(hour, temperature)
 
-        home.addWidget(self.temporary)
+        home.addWidget(self.temporary_graph)
         home.addWidget(plot_button_splitter)
         home.addWidget(home_footer)
         home.setSizes([300, 50])
@@ -77,14 +83,48 @@ class Body(QWidget):
         self.y = 0
 
     def update(self):  
+        current_plot = self.plot_buttons.getCurrentPlot()
+        pen = pg.mkPen(width=10)
+
         if (self.plot_buttons.getPlotResetFlag()):
-            self.x = 0
-            self.y = 0
+            # Reset the current plot
+            self.current_plot_values[current_plot][0] = 0
+            self.current_plot_values[current_plot][1] = 0
+            self.current_plot_indices[current_plot] = 0
+            self.x_data[current_plot] = [0]
+            self.y_data[current_plot] = [0]
             self.plot_buttons.setPlotResetFlag(False)
-        else:
-            pen = pg.mkPen(width=10)
-            self.temporary.plot([self.x], [self.y],
+
+        elif (self.plot_buttons.getChangedPlot()):
+            # Reset the plot
+            self.temporary_graph.clear()
+
+            # Plot all the data for the new plot
+            for i in range(len(self.x_data[current_plot])):
+                self.temporary_graph.plot([self.x_data[current_plot][i]], [self.y_data[current_plot][i]],
                                 pen=pen, symbol='x', symbolSize=30)
-            self.x += 1
-            self.y += 1
+
+            # Reset the changed plot flag
+            self.plot_buttons.setChangedPlot(False)
+    
+        else:
+            # Updated variables for the next data
+            for i in range(NUM_PLOTS):
+                self.x_data[i].append(self.current_plot_values[i][0])
+                self.y_data[i].append(self.current_plot_values[i][1])
+                self.current_plot_values[i][0] += PLOT_INCREMENTS[i]
+                self.current_plot_values[i][1] += PLOT_INCREMENTS[i]
+
+            # Get the current index
+            currentIndex = self.current_plot_indices[current_plot]
+
+            # Plot the current data point
+            self.temporary_graph.plot([self.x_data[current_plot][currentIndex]], [self.y_data[current_plot][currentIndex]],
+                                pen=pen, symbol='x', symbolSize=30)
+            
+            # Update the indices for the next datapoint
+            self.current_plot_indices[0] += 1
+            self.current_plot_indices[1] += 1
+            
+
 
--- GUI/widgets/plot_buttons.py
+++ GUI/widgets/plot_buttons.py
@@ -7,6 +7,9 @@ class PlotButtons(QWidget):
         super(PlotButtons, self).__init__(None)
         self.plot = plot
         self.plot_reset = False
+        self.current_plot = 0
+        self.plot_names = ['Plot 1', 'Plot 2']
+        self.changed_plot = False
         self.initUI()
 
     def initUI(self):
@@ -23,8 +26,18 @@ class PlotButtons(QWidget):
         self.example_button.setText("Example Button")
         self.example_button.setStyleSheet("font-family: Helvetica; font-size: 14px; background-color : #2B26c1")
 
+        self.plot_dropdown = QComboBox()
+        self.plot_dropdown.setEditable(True)
+        self.plot_dropdown.lineEdit().setAlignment(Qt.AlignCenter)
+        self.plot_dropdown.lineEdit().setReadOnly(True)
+        for plot_name in self.plot_names:
+            self.plot_dropdown.addItem(plot_name)
+        self.plot_dropdown.setStyleSheet("font-family: Helvetica; font-size: 14px; background-color : #2B26c1;")
+        self.plot_dropdown.currentTextChanged.connect(self.plotDropdownChanged)
+       
         hbox.addWidget(self.reset_plot_button)
         hbox.addWidget(self.example_button)
+        hbox.addWidget(self.plot_dropdown)
         self.setLayout(hbox)
 
     def resetPlot(self):
@@ -36,3 +49,16 @@ class PlotButtons(QWidget):
     
     def getPlotResetFlag(self):
         return self.plot_reset
+
+    def plotDropdownChanged(self, dropdownValue):
+        self.current_plot = self.plot_names.index(dropdownValue)
+        self.changed_plot = True
+
+    def getCurrentPlot(self):
+        return self.current_plot
+
+    def getChangedPlot(self):
+        return self.changed_plot
+    
+    def setChangedPlot(self, newValue):
+        self.changed_plot = newValue
