64b0905c46a0ad5aecea76b52eb8677574bfcea3
mlo20030 <morganelizabeth03@gmail.com>
2022-10-31 18:17:36 -0400

    working on button

--- GUI/widgets/plot_buttons.py
+++ GUI/widgets/plot_buttons.py
@@ -1,13 +1,17 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+import pandas as pd
 
 class PlotButtons(QWidget):
-    def __init__(self, plot):
+    def __init__(self, plot, xdata, ydata):
         super(PlotButtons, self).__init__(None)
         self.plot = plot
         self.plot_reset = False
         self.initUI()
+        self.downloaded = False
+        self.xdata= xdata
+        self.ydata= ydata
 
     def initUI(self):
 
@@ -23,9 +27,25 @@ class PlotButtons(QWidget):
         self.example_button.setText("Example Button")
         self.example_button.setStyleSheet("font-family: Helvetica; font-size: 14px; background-color : #2B26c1")
 
+        self.downloadButton = QPushButton(self)
+        self.downloadButton.setText("reset Plot")
+        self.downloadButton.setStyleSheet("font-family: Helvetica; font-size: 14px; background-color : #2B26c1")
+        self.downloadButton.clicked.connect(self.downloadData)
+
         hbox.addWidget(self.reset_plot_button)
         hbox.addWidget(self.example_button)
+        hbox.addWidget(self.downloadButton)
         self.setLayout(hbox)
+
+
+    def downloadData(self):
+        self.zipped = zip(self.xdata, self.ydata)
+        df = pd.DataFrame(self.zipped)
+        df.to_csv
+        self.downloaded = True
+    
+    @st.cache
+        return 
 
     def resetPlot(self):
         self.plot.clear()
