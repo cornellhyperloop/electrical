41d366adfbad230ff3a56d968dc16ee2643ebebe
rwm275 <rwm275@cornell.edu>
2022-10-31 18:45:09 -0400

    asdfg

--- GUI/widgets/plot_buttons.py
+++ GUI/widgets/plot_buttons.py
@@ -3,15 +3,14 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import pandas as pd
 
+
 class PlotButtons(QWidget):
-    def __init__(self, plot, xdata, ydata):
+    def __init__(self, plot):
         super(PlotButtons, self).__init__(None)
         self.plot = plot
         self.plot_reset = False
         self.initUI()
         self.downloaded = False
-        self.xdata= xdata
-        self.ydata= ydata
 
     def initUI(self):
 
@@ -20,16 +19,19 @@ class PlotButtons(QWidget):
         # Reset plot button
         self.reset_plot_button = QPushButton(self)
         self.reset_plot_button.setText("Reset Plot")
-        self.reset_plot_button.setStyleSheet("font-family: Helvetica; font-size: 14px; background-color : #2B26c1")
+        self.reset_plot_button.setStyleSheet(
+            "font-family: Helvetica; font-size: 14px; background-color : #2B26c1")
         self.reset_plot_button.clicked.connect(self.resetPlot)
 
         self.example_button = QPushButton(self)
         self.example_button.setText("Example Button")
-        self.example_button.setStyleSheet("font-family: Helvetica; font-size: 14px; background-color : #2B26c1")
+        self.example_button.setStyleSheet(
+            "font-family: Helvetica; font-size: 14px; background-color : #2B26c1")
 
         self.downloadButton = QPushButton(self)
         self.downloadButton.setText("Download Data")
-        self.downloadButton.setStyleSheet("font-family: Helvetica; font-size: 14px; background-color : #2B26c1")
+        self.downloadButton.setStyleSheet(
+            "font-family: Helvetica; font-size: 14px; background-color : #2B26c1")
         self.downloadButton.clicked.connect(self.downloadData)
 
         hbox.addWidget(self.reset_plot_button)
@@ -37,16 +39,15 @@ class PlotButtons(QWidget):
         hbox.addWidget(self.downloadButton)
         self.setLayout(hbox)
 
-
     def downloadData(self):
         self.plot.getPlotItem().writeCsv("Data.csv")
 
     def resetPlot(self):
         self.plot.clear()
         self.plot_reset = True
-    
+
     def setPlotResetFlag(self, flag):
         self.plot_reset = flag
-    
+
     def getPlotResetFlag(self):
         return self.plot_reset
