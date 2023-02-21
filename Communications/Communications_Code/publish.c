f2ad08b8fe07966570eba6048780e9b4662ab1a3
Vanshaj Jain <vj53@cornell.edu>
2023-02-21 18:25:44 -0500

    Fixed publish.c to send stub values

--- Communications/Communications_Code/publish.c
+++ Communications/Communications_Code/publish.c
@@ -6,8 +6,6 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <termios.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
 
@@ -65,6 +63,17 @@ int main(int argc, char *argv[])
 	/* Flush anything already in the serial buffer */
 	tcflush(fd, TCIFLUSH);
 
+	msg.imu_acceleration_x = 1.0;
+	msg.imu_acceleration_y = 2.0;
+	msg.imu_acceleration_z = 3.0;
+	msg.imu_gyroscope = 4.0;
+	msg.imu_magnetometer = 5.0;
+	msg.pressure = 6.0;
+	msg.temperature = 32.0;
+	// msg.temperature = atof(buf); // TODO: Get value from arduino
+	msg.proximity = 8.0;
+	msg.distance = 9.0;
+
 	while (stop == 0)
 	{
 
@@ -76,7 +85,7 @@ int main(int argc, char *argv[])
 		/* print what's in the buffer */
 		printf("Buffer contains...\n%s\n", buf);
 
-		msg.temperature = atof(buf); // TODO: Get value from arduino
+		// msg.temperature = atof(buf); // TODO: Get value from arduino
 		sensor_info_t_publish(zcm, "SENSOR_INFO", &msg);
 	}
 
@@ -92,16 +101,6 @@ int main(int argc, char *argv[])
 	//   float proximity;
 	//   float distance;
 	// }
-
-	msg.imu_acceleration_x = 1.0;
-	msg.imu_acceleration_y = 2.0;
-	msg.imu_acceleration_z = 3.0;
-	msg.imu_gyroscope = 4.0;
-	msg.imu_magnetometer = 5.0;
-	msg.pressure = 6.0;
-	msg.temperature = atof(buf); // TODO: Get value from arduino
-	msg.proximity = 8.0;
-	msg.distance = 9.0;
 
 	while (1)
 	{
