dae3c954350f6a7d3f4b882f187e0bd54538149f
Stephen Chien <slc292@cornell.edu>
2023-09-25 18:42:56 -0400

    added more to the competition page

--- Website/src/components/Competition.jsx
+++ Website/src/components/Competition.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../partials/Header";
-import Newsletter from "../partials/Newsletter";
 import Footer from "../partials/Footer";
 import "../Timeline.css";
 import OldTeam from "../img/oldteam.png";
-
+import fun from "../img/fun.jpg";
+import teamchc from "../img/team_CHC.jpg";
+import powershot1 from "../img/power-shot1.png";
+import powershot2 from "../img/power-shot2.png"
 
 function CompetitionPage() {
   return (
@@ -20,8 +22,9 @@ function CompetitionPage() {
             <h1 className="text-3xl font-semibold mb-8">
               Cornell Hyperloop in the News
             </h1>
-            <p className="text-gray-600">
-              Cornell Daily Sun wrote an article about Cornell Hyperloop's recent achievements in competitions. You can read the full article by clicking the link below:
+            <p className="text-black-600">
+              Cornell Daily Sun wrote an article about Cornell Hyperloop's recent achievements in competitions.
+               You can read the full article by clicking the link below:
             </p>
             <p className="mt-4">
               <a
@@ -33,6 +36,35 @@ function CompetitionPage() {
                 Read the article
               </a>
             </p>
+            <p className="mt-4">
+                <a>We plan to attend more competitions in the near future. Stay in touch!</a>
+
+
+
+            </p>
+
+          </div>
+        </section>
+
+        <section className="py-16">
+          <div className="container mx-auto px-4">
+            <h2 className="text-2xl font-semibold mb-4">Photo Gallery</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {/* Add your gallery images here */}
+              <div className="rounded-lg p-4 bg-white">
+                <img src={fun} alt="Gallery Image 1" className="w-full h-full" />
+              </div>
+              <div className="rounded-lg p-4 bg-white">
+                <img src={teamchc} alt="Gallery Image 2" className="w-full h-auto" />
+              </div>
+              <div className="rounded-lg p-4 bg-white">
+                <img src={powershot1} alt="Gallery Image 3" className="w-full h-auto" />
+              </div>
+              <div className="rounded-lg p-4 bg-white">
+                <img src={powershot2} alt="Gallery Image 3" className="w-full h-auto" />
+              </div>
+              {/* Add more images as needed */}
+            </div>
           </div>
         </section>
       </main>
