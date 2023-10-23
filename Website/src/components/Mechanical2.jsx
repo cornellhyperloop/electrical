33f83c35455240a29645301acef5161fc7a34525
Stephen Chien <slc292@cornell.edu>
2023-10-23 18:48:23 -0400

    Updated Mechanical2.jsx in our components

--- Website/src/components/Mechanical2.jsx
+++ Website/src/components/Mechanical2.jsx
@@ -40,94 +40,26 @@ function FeaturesBlocks() {
         aria-hidden="true"
       ></div>
 
+      
+
+
+
+
+
+
+
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20" id="mechanical">
+
           {/* Section header */}
           <div className=" mx-auto flex justify-between text-center pb-8 md:pb-8">
             <h2 className="h3 mb-2 text-4xl font-bold">Subteams</h2>
             <div></div>
           </div>
 
-          <div className="md:grid md:grid-cols-12 md:gap-6">
-            {/* Content */}
-            <div
-              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
-              data-aos="fade-right"
-            >
-              <div className="md:pr-4 bg-gray-50 p-3 rounded-sm lg:pr-12 xl:pr-16 ">
-                <h3 className="h3 my-1 font-light text-xl">Propulsion</h3>
-                <h3 className="h3 my-1 ">
-                  The propulsion subteam works on our propulsion system and
-                  fuselage.
-                </h3>
-                <h3 className="h3 mt-8 mb-1 font-light text-xl">Suspension</h3>
-                <h3 className="h3 my-1 ">
-                  The suspension team focuses on the pod's suspension mechanisms
-                  and chassis design.
-                </h3>
-                <h3 className="h3 mt-8 mb-1 font-light text-xl">Braking</h3>
-                <h3 className="h3 my-1 ">
-                  The braking team specializes within the mechanical and
-                  magnetic braking systems.
-                </h3>
-              </div>
 
 
-              {/* Tabs buttons */}
-            </div>
-
-            <div
-              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
-              data-aos="fade-right"
-            >
-              <div className="md:pr-4 bg-gray-50 p-3 rounded-sm lg:pr-12 xl:pr-16 ">
-                <h3 className="h3 my-1 font-light text-xl">Propulsion</h3>
-                <h3 className="h3 my-1 ">
-                  The propulsion subteam works on our propulsion system and
-                  fuselage.
-                </h3>
-                <h3 className="h3 mt-8 mb-1 font-light text-xl">Suspension</h3>
-                <h3 className="h3 my-1 ">
-                  The suspension team focuses on the pod's suspension mechanisms
-                  and chassis design.
-                </h3>
-                <h3 className="h3 mt-8 mb-1 font-light text-xl">Braking</h3>
-                <h3 className="h3 my-1 ">
-                  The braking team specializes within the mechanical and
-                  magnetic braking systems.
-                </h3>
-              </div>
-
-
-              {/* Tabs buttons */}
-            </div>
-
-            <div
-              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
-              data-aos="fade-right"
-            >
-              <div className="md:pr-4 bg-gray-50 p-3 rounded-sm lg:pr-12 xl:pr-16 ">
-                <h3 className="h3 my-1 font-light text-xl">Propulsion</h3>
-                <h3 className="h3 my-1 ">
-                  The propulsion subteam works on our propulsion system and
-                  fuselage.
-                </h3>
-                <h3 className="h3 mt-8 mb-1 font-light text-xl">Suspension</h3>
-                <h3 className="h3 my-1 ">
-                  The suspension team focuses on the pod's suspension mechanisms
-                  and chassis design.
-                </h3>
-                <h3 className="h3 mt-8 mb-1 font-light text-xl">Braking</h3>
-                <h3 className="h3 my-1 ">
-                  The braking team specializes within the mechanical and
-                  magnetic braking systems.
-                </h3>
-              </div>
-
-
-              {/* Tabs buttons */}
-            </div>
-
+          <div className="mt-4"></div>
             {/* Tabs items */}
             <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6  md:mb-0 md:order-1">
               <div className="md:pr-4 lg:pr-12 xl:pr-16 bg-gray-50 p-3 rounded-sm ">
@@ -147,6 +79,65 @@ function FeaturesBlocks() {
                 </h3>
               </div>{" "}
             </div>
+
+            <div className="mt-4"></div>
+
+          <div className="md:grid md:grid-cols-12 md:gap-6">
+            {/* Content */}
+            <div
+              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+              data-aos="fade-right"
+            >
+              <div className="md:pr-4 bg-gray-50 p-3 rounded-sm lg:pr-12 xl:pr-16 ">
+                <h3 className="h3 my-1 font-light text-xl">Propulsion</h3>
+                <h3 className="h3 my-1 ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident,
+                   sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </h3>
+              </div>
+
+
+              {/* Tabs buttons */}
+            </div>
+
+            <div
+              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+              data-aos="fade-right"
+            >
+              <div className="md:pr-4 bg-gray-50 p-3 rounded-sm lg:pr-12 xl:pr-16 ">
+                <h3 className="h3 my-1 font-light text-xl">Suspension</h3>
+                <h3 className="h3 my-1 ">
+                The suspension team focuses on the pod's suspension mechanisms
+                  and chassis design.
+                </h3>
+
+              </div>
+
+
+              {/* Tabs buttons */}
+            </div>
+
+            <div
+              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+              data-aos="fade-right"
+            >
+              <div className="md:pr-4 bg-gray-50 p-3 rounded-sm lg:pr-12 xl:pr-16 ">
+
+                <h3 className="h3 mt-8 mb-1 font-light text-xl">Braking</h3>
+                <h3 className="h3 my-1 ">
+                  The braking team specializes within the mechanical and
+                  magnetic braking systems.
+                </h3>
+              </div>
+
+
+              {/* Tabs buttons */}
+            </div>
+          
           </div>
 
 
