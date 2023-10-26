d552168501135293248a50916ce528e46370d31a
dk556cornell <dk556@cornell.edu>
2023-10-26 16:28:39 -0400

    Electrical and Business page paths

--- Website/src/App.js
+++ Website/src/App.js
@@ -6,8 +6,9 @@ import Members from './components/Members';
 import Team from './components/Team';
 import Apply from './components/Apply';
 import Mechanical2 from './components/Mechanical2'
-
-import { Route,Routes,BrowserRouter} from 'react-router-dom';
+import Electrical from './components/Electrical'
+import Business from './components/Business'
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Donate from './components/Donate';
 import CompetitionPage from './components/Competition';
 
@@ -16,21 +17,24 @@ function App() {
 
   return (
     <div className="App" class="p-0 items-center justify-center">
-          <Routes>
-        <Route path="/" element={  <HomePage/>}  >
-        
-          </Route>
-          <Route  path="/members" element={<Members/>}/>
-          <Route  path="/team" element={<Team/>}/>
-          <Route  path="/apply" element={<Apply/>}/>
-          <Route  path="/donate" element={<Donate/>}/>
-          <Route  path="/competition" element={<CompetitionPage/>}/>
-          <Route  path="/mechanical" element={<Mechanical2/>}/>
-          
+      <Routes>
+        <Route path="/" element={<HomePage />}  >
 
-          </Routes>
+        </Route>
+        <Route path="/members" element={<Members />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/apply" element={<Apply />} />
+        <Route path="/donate" element={<Donate />} />
+        <Route path="/competition" element={<CompetitionPage />} />
+        <Route path="/mechanical" element={<Mechanical2 />} />
+        <Route path="/electrical" element={<Electrical />} />
+        <Route path="/business" element={<Business />} />
 
-      </div>
+
+
+      </Routes>
+
+    </div>
   );
 }
 
---
+++ Website/src/components/Business.jsx
@@ -0,0 +1,245 @@
+import React, { useState } from "react";
+import HeroImage from "../img/logo_symbol.png";
+
+import Header from "../partials/Header";
+import FeaturesHome from "../partials/Features";
+import Testimonials from "../partials/Testmonials";
+import Newsletter from "../partials/Newsletter";
+import Footer from "../partials/Footer";
+import Banner from "../partials/Banner";
+import Challenge from "../partials/Challenge";
+import Business from "../img/business.png";
+import Mechanical from "../img/mechanical.png";
+import Electrical from "../img/electrical.png";
+
+function Business2() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      {/*  Site header */}
+      <Header />
+      {/*  Page content */}
+      <main className="flex-grow">
+        {/*  Page sections */}
+        <HeroMembers />
+        {FeaturesBlocks()}
+      </main>
+      <Banner />
+
+      {/*  Site footer */}
+    </div>
+  );
+}
+
+function FeaturesBlocks() {
+
+  return (
+    <section className="relative">
+      {/* Section background (needs .relative class on parent and next sibling elements) */}
+      <div
+        className="absolute inset-0 bottom-1/2 md:mt-4 lg:mt-0  pointer-events-none"
+        aria-hidden="true"
+      ></div>
+
+      <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20" id="business">
+          {/* Section header */}
+          <div className=" mx-auto flex justify-between text-center pb-8 md:pb-8">
+            <h2 className="h3 mb-2 text-4xl font-bold">Subteams</h2>
+            <div></div>
+          </div>
+
+          <div className="md:grid md:grid-cols-12 md:gap-6">
+            {/* Content */}
+            <div
+              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+              data-aos="fade-right"
+            >
+              <div className="md:pr-4 bg-gray-50 p-3 rounded-sm lg:pr-12 xl:pr-16 ">
+                <h3 className="h3 my-1 font-light text-xl">Operations</h3>
+                <h3 className="h3 my-1 ">
+                  The operations subteam manages budgeting, procurement,
+                  sponsorship outreach, and fundraising.
+                </h3>
+                <h3 className="h3 mt-8 mb-1 font-light text-xl">Marketing</h3>
+                <h3 className="h3 my-1 ">
+                  The marketing subteam maintains our team’s social media presence
+                  and is also jointly responsible for the website.
+                </h3>
+              </div>
+
+
+              {/* Tabs buttons */}
+            </div>
+
+            <div
+              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+              data-aos="fade-right"
+            >
+              <div className="md:pr-4 bg-gray-50 p-3 rounded-sm lg:pr-12 xl:pr-16 ">
+                <h3 className="h3 my-1 font-light text-xl">Operations</h3>
+                <h3 className="h3 my-1 ">
+                  The operations subteam manages budgeting, procurement,
+                  sponsorship outreach, and fundraising.
+                </h3>
+                <h3 className="h3 mt-8 mb-1 font-light text-xl">Marketing</h3>
+                <h3 className="h3 my-1 ">
+                  The marketing subteam maintains our team’s social media presence
+                  and is also jointly responsible for the website.
+                </h3>
+              </div>
+
+
+              {/* Tabs buttons */}
+            </div>
+
+            <div
+              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+              data-aos="fade-right"
+            >
+              <div className="md:pr-4 bg-gray-50 p-3 rounded-sm lg:pr-12 xl:pr-16 ">
+                <h3 className="h3 my-1 font-light text-xl">Operations</h3>
+                <h3 className="h3 my-1 ">
+                  The operations subteam manages budgeting, procurement,
+                  sponsorship outreach, and fundraising.
+                </h3>
+                <h3 className="h3 mt-8 mb-1 font-light text-xl">Marketing</h3>
+                <h3 className="h3 my-1 ">
+                  The marketing subteam maintains our team’s social media presence
+                  and is also jointly responsible for the website.
+                </h3>
+              </div>
+
+
+              {/* Tabs buttons */}
+            </div>
+
+            {/* Tabs items */}
+            <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6  md:mb-0 md:order-1">
+              <div className="md:pr-4 lg:pr-12 xl:pr-16 bg-gray-50 p-3 rounded-sm ">
+                <h3 className="h3 my-1 font-light text-xl">Description</h3>
+                <h3 className="h3 my-1 ">
+                  The business team is responsible for supporting the team’s
+                  business-related initiatives, including leading multiple
+                  initiatives to fund the development of our pod.
+
+
+                </h3>
+                <h3 className="h3 mt-8 mb-1 font-light text-xl">
+                  Tools & Tech
+                </h3>
+                <h3 className="h3 my-1 ">
+                  The business team uses Adobe Creative Cloud to produce marketing
+                  materials for the website and our social media. We also use
+                  Excel and spreadsheets to manage the team’s finances.
+                </h3>
+              </div>{" "}
+            </div>
+          </div>
+
+
+          {/* Items */}
+        </div>
+
+        {/* Items */}
+      </div>
+    </section>
+  );
+}
+function Member({ src, title, subtitle }) {
+  return (
+    <div className="relative flex flex-col items-center mx-4 bg-white rounded ">
+      <img
+        className="h-44 w-44 rounded-md"
+        src={require("../img/" + src).default}
+      ></img>
+
+      <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 mt-4 self-start">
+        {title}
+      </h4>
+      <h4 className="text-lg font-normal leading-snug tracking-tight mb-1 mt-1 self-start">
+        {subtitle}
+      </h4>
+    </div>
+  );
+}
+function HeroMembers(myRef) {
+  const [videoModalOpen, setVideoModalOpen] = useState(false);
+
+  return (
+    <section className="relative max-h-screen">
+      {/* Illustration behind hero content */}
+      <div
+        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
+        aria-hidden="true"
+      >
+        <svg
+          width="1360"
+          height="578"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01"
+            >
+              <stop stopColor="#FFF" offset="0%" />
+              <stop stopColor="#EAEAEA" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
+        </svg>
+      </div>
+
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        {/* Hero content */}
+        <div className="pt-32 pb-12 md:pt-36 md:pb-20">
+          {/* Section header */}
+
+          {/* Hero image */}
+          <div>
+            <div
+              className="relative flex justify-center mb-8 h-full"
+              data-aos="zoom-y-out"
+              data-aos-delay="450"
+            >
+              <div className="flex flex-col justify-center ">
+                <div className=" w-screen items-center justify-center">
+                  <img
+                    className="mx-auto w-1/4 h-full"
+                    src={HeroImage}
+                    alt="Hero"
+                  />
+                </div>
+                <svg
+                  className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto"
+                  width="768"
+                  height="432"
+                  viewBox="0 0 768 432"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                ></svg>
+              </div>
+            </div>
+          </div>
+          <div className="text-center pb-12 md:pb-0">
+            <h1
+              className="text-5xl md:text-6xl font-extrabold mb-4"
+              data-aos="zoom-y-out"
+            >
+              Business
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Business2;
