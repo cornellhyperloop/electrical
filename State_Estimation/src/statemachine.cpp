ddf3802a7197d3707ee7bf19cfb7e52a5993d7fc
Levi Zeng <lezeng003006@gmail.com>
2024-04-15 19:48:04 -0400

    Linux Serial Class Work

--- State_Estimation/src/statemachine.cpp
+++ State_Estimation/src/statemachine.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "stateMachineFuncs.cpp"
+#include "LinuxSerialClass.cpp"
 // TODO: Fix FSM logic
 
 // int main()
@@ -13,7 +14,7 @@
 
 //   while (1)
 //   {
-  // make sure to check for emergency input
+// make sure to check for emergency input
 //     switch (curr)
 //     {
 //     case Verification:
@@ -78,12 +79,43 @@ int main()
   states curr = Verification;
   states prev = Verification;
   // traveledDist = Serial Read for LIDAR to get Traveled Distance
-  double traveledDist = 0.0; 
+  double traveledDist = 0.0;
   double sensorVelocity = 0.0;
+
+  Serial serial("/dev/ttyUSB0");
 
   while (prev != Stop)
   {
-  //make sure to check for emergency input
+    // make sure to check for emergency input
+    char serialData[256];
+    int bytesRead = serial.ReadData(serialData, sizeof(serialData));
+
+    // parse byteRead
+
+    // Process the serial data if any bytes were read
+    if (bytesRead > 0)
+    {
+      // Parse the received data
+      for (int i = 0; i < bytesRead; ++i)
+      {
+        char dataByte = serialData[i];
+        // Perform parsing based on your protocol or data format
+        // For example, if the data is ASCII-encoded integers separated by commas:
+        // You can use strtok to tokenize the data
+        char *token = strtok(serialData, ",");
+        while (token != NULL)
+        {
+          // Convert the token to integer if needed
+          int value = atoi(token);
+          // Process the integer value
+          printf("Received value: %d\n", value);
+          // Move to the next token
+          token = strtok(NULL, ",");
+        }
+      }
+    }
+
+    // parse bytesRead
     switch (curr)
     {
     case Verification:
@@ -92,8 +124,8 @@ int main()
       /* Task 2: Make a verifySensors function to check they're all on,
                   and ensure readings are in a reasonable range.
       **/
-     double t1[1] = {1.0};
-     double t3[1] = {1.0};
+      double t1[1] = {1.0};
+      double t3[1] = {1.0};
       //  Update function call for verifySensors() with  appropriate parameters
       curr = verifySensors(t1, 0.0, t3, 0.0); // CHANGE
       // curr = verifySensors(std::get<0>(readData()), stubValue);
