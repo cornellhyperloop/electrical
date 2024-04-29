a85135938a098c6042537fa13fa9f8da738002fd
maxxfarma <148791889+maxxfarma@users.noreply.github.com>
2024-04-29 19:02:14 -0400

    Create cooling_systems.py

---
+++ GUI/widgets/cooling_systems.py
@@ -0,0 +1,38 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import constants
+
+class CoolingSystem(QWidget):
+    def __init__(self,parent = None):
+        super(CoolingSystem,self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        system1 = Qlabel(self)
+        system2 = Qlabel(self)
+        system3 = Qlabel(self)
+
+        system1.setText("Cooling System 1: " +
+                        constants.SYSTEM1_STATE)
+        system1.setAlignment(Qt.AlignCenter)
+        sensor1.setStyleSheet("background-color : #2B26c1")
+
+        system2.setText("Cooling System 2: " +
+                        constants.SYSTEM2_STATE)
+        system2.setAlignment(Qt.AlignCenter)
+        sensor2.setStyleSheet("background-color : #2B26c1")
+
+        system3.setText("Cooling System 2: " +
+                        constants.SYSTEM3_STATE)
+        system3.setAlignment(Qt.AlignCenter)
+        sensor3.setStyleSheet("background-color : #2B26c1")
+        
+        
+        vbox = QVBoxLayout()
+        vbox.addWidget(system1)
+        vbox.addWidget(system2)
+        vbox.addWidget(system3)
+
+        self.setLayout(vbox)
+
