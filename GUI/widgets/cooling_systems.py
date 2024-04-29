72b27bcccd5f11d4e4d8413915045061a2145122
aislinnennis <116472948+aislinnennis@users.noreply.github.com>
2024-04-29 19:33:48 -0400

    Update cooling_systems.py

--- GUI/widgets/cooling_systems.py
+++ GUI/widgets/cooling_systems.py
@@ -2,7 +2,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import constants
-import sys
 
 class CoolingSystem(QWidget):
     def __init__(self,parent = None):
