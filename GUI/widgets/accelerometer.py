e1159d32713da4d9435972cd97ee74421ac1a2e9
Levi Zeng <lezeng003006@gmail.com>
2024-09-21 11:38:26 -0400

    Relevant widgets commented

--- GUI/widgets/accelerometer.py
+++ GUI/widgets/accelerometer.py
@@ -7,28 +7,57 @@ import constants
 
 class Accelerometer(QWidget):
     def __init__(self, parent=None):
+        """
+        Initializes the Accelerometer widget.
+
+        Parameters:
+            parent (QWidget): The parent widget for this accelerometer display.
+        """
         super(Accelerometer, self).__init__(parent)
-        self.initUI()
+        self.initUI()  # Call the method to set up the UI
 
     def initUI(self):
-        self.dial = QDial(self)
-        self.dial.setMinimum(0)
-        self.dial.setMaximum(100)
-        self.dial.setValue(constants.ACCELERATION)
+        """
+        Sets up the user interface for the Accelerometer widget.
+
+        This method creates a QDial to visually represent the current acceleration. 
+        The dial is initialized with a minimum and maximum value, and its initial 
+        value is set based on the current acceleration constant. The dial's 
+        background color is also set based on whether the acceleration exceeds a 
+        predefined threshold. A vertical layout is created to hold the dial, 
+        and a timer is set to update the display every second.
+        """
+        self.dial = QDial(self)  # Create a dial for acceleration display
+        self.dial.setMinimum(0)  # Set minimum value of the dial
+        self.dial.setMaximum(100)  # Set maximum value of the dial
+        self.dial.setValue(constants.ACCELERATION)  # Set initial value of the dial
+        
+        # Change the background color based on the acceleration threshold
         if constants.ACCELERATION > constants.ACCELERATION_THRESHOLD:
             self.dial.setStyleSheet("background-color : red")
         else:
             self.dial.setStyleSheet("background-color : green")
 
-        vbox = QVBoxLayout()
+        vbox = QVBoxLayout()  # Create a vertical box layout for the dial
 
-        vbox.addWidget(self.dial)
-        self.setLayout(vbox)
+        vbox.addWidget(self.dial)  # Add the dial to the layout
+        self.setLayout(vbox)  # Set the layout for this widget
+        
+        # Set up a QTimer to regularly call the update method
         self.timer = QTimer(self, timeout=self.update)
-        self.timer.start(1000)
+        self.timer.start(1000)  # Timer set to call update() every second
 
     def update(self):
-        constants.ACCELERATION = self.dial.value()
+        """
+        Updates the accelerometer dial based on the current value.
+
+        This method retrieves the current value from the dial and updates the 
+        acceleration constant. It also adjusts the dial's background color based 
+        on whether the current value exceeds the predefined acceleration threshold.
+        """
+        constants.ACCELERATION = self.dial.value()  # Update the constant with the current dial value
+        
+        # Change the background color based on the acceleration threshold
         if self.dial.value() > constants.ACCELERATION_THRESHOLD:
             self.dial.setStyleSheet("background-color : red")
         else:
--- GUI/widgets/help_popup.py
+++ GUI/widgets/help_popup.py
@@ -6,28 +6,57 @@ import sys
 
 class HelpPopup(QWidget):
     def __init__(self, *args, parent=None):
+        """
+        Initializes the HelpPopup widget.
+
+        Parameters:
+            args (tuple): Additional arguments for width and height.
+            parent (QWidget): The parent widget for this help popup.
+        """
         super(HelpPopup, self).__init__(parent)
-        self.width = args[0] / 10
-        self.height = args[1] / 30
-        self.initUI()
+        self.width = args[0] / 10  # Set the width based on passed arguments
+        self.height = args[1] / 30  # Set the height based on passed arguments
+        self.initUI()  # Call the method to set up the UI
 
     def initUI(self):
-        self.setWindowTitle("Click button")
-        self.push = QPushButton(self)
-        self.push.setText("Help")
-        self.push.setFont(QFont('AnyStyle', 18))
+        """
+        Sets up the user interface for the HelpPopup widget.
+
+        This method initializes the help button with specific text, font, and styles.
+        It connects the button's click event to the `pushedHelp` method to display 
+        the help message.
+        """
+        self.setWindowTitle("Click button")  # Set the window title
+        self.push = QPushButton(self)  # Create the help button
+        self.push.setText("Help")  # Set button text
+        self.push.setFont(QFont('AnyStyle', 18))  # Set button font size
+        
+        # Style the button with a custom background color and other visual properties
         self.push.setStyleSheet(
             "background-color : #2B26c1; border-radius: 5px; font-family: Helvetica; font-size: 14px; border: 3px #2B26c1")
-        self.push.clicked.connect(self.pushedHelp)
-        self.push.resize(int(self.width), int(self.height))
+        
+        self.push.clicked.connect(self.pushedHelp)  # Connect button click to help function
+        self.push.resize(int(self.width), int(self.height))  # Set button size
 
     def pushedHelp(self):
-        self.msgBox = QMessageBox()
-        self.msgBox.setWindowTitle("Tutorial on using GUI")
+        """
+        Displays a help message when the help button is clicked.
+
+        This method creates a message box that provides information on how to use 
+        the GUI, encouraging users to click on various buttons for more details.
+        """
+        self.msgBox = QMessageBox()  # Create a message box instance
+        self.msgBox.setWindowTitle("Tutorial on using GUI")  # Set message box title
         self.msgBox.setText(
-            "Click on the various buttons in order to receive more information about them")
-        self.msgBox.setIcon(QMessageBox.Information)
-        x = self.msgBox.exec()
+            "Click on the various buttons in order to receive more information about them")  # Set the message text
+        self.msgBox.setIcon(QMessageBox.Information)  # Set the icon for the message box
+        x = self.msgBox.exec()  # Execute the message box and wait for user response
 
     def sizeHint(self):
-        return QSize(int(self.width), int(self.height))
+        """
+        Provides the recommended size for the HelpPopup widget.
+
+        Returns:
+            QSize: The recommended size based on the specified width and height.
+        """
+        return QSize(int(self.width), int(self.height))  # Return the size hint
